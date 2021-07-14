bfaaaf2f41a7ef65798252bfb860b59e12b06bab
krjw-00 <kriswd0@gmail.com>
2021-07-15 11:56:14 +1200

    DB function - addUser

--- server/db/users.js
+++ server/db/users.js
@@ -1,9 +1,31 @@
 const connection = require('./connection')
 
-function getUsers (db = connection) {
+module.exports = {
+  getUser,
+  addUser,
+  editProfile,
+  deleteAccount
+}
+
+function getUser (db = connection) {
   return db('users').select()
 }
 
-module.exports = {
-  getUsers
+function addUser (newUser, db = connection) {
+  const dateCreated = Date.now() //what is the format of this date?
+  const { name, location, isCompany, email, auth0id } = newUser
+  return db('users')
+  .insert({ 
+    name,
+    dateCreated, 
+    location, 
+    isCompany, 
+    email, 
+    auth0id 
+  })
+  .then((ids) => console.log(ids[0]))
 }
+
+function editProfile (db = connection) {}
+
+function deleteAccount (db = connection) {}
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,16 +4,31 @@ const db = require('../db/users')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  db.getUsers()
-    .then(results => {
-      res.json({ users: results.map(user => user.name) })
-      return null
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ message: 'Somthing went wrong' })
-    })
-})
+// GET fetchUser - for profile page 
+// router.get('/', (req, res) => {
+//   db.getUser()
+//     .then(results => {
+//       res.json({ users: results.map(user => user.name) })
+//       return null
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(500).json({ message: 'Somthing went wrong' })
+//     })
+// })
+
+// POST registerUser
+// router.post('/', (req, res) => {
+
+//   db.addUser()
+//     .then(results => {
+//       res.json({ users: results.map(user => user.name) })
+//       return null
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(500).json({ message: 'Somthing went wrong' })
+//     })
+// })
 
 module.exports = router
