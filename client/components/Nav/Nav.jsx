73e42867b427461e6d4ae7b335162510229daeef
Zachary Choi <zach7choi@gmail.com>
2021-07-15 11:35:31 +1200

    seperate components into folders, setup App.jsx

---
+++ client/components/Nav/Nav.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+// import { useAuth0 } from '@auth0/auth0-react'
+
+// import {
+//   IfAuthenticated,
+//   IfNotAuthenticated
+// } from '../Authenticated/Authenticated'
+// import {
+//   getLoginFn,
+//   getLogoutFn,
+//   getRegisterFn
+// } from '../../auth-utils'
+
+// // React-Icons Import
+// import { GiHamburgerMenu } from 'react-icons/gi'
+// import { IoClose } from 'react-icons/io5'
+
+export default function Nav () {
+  const [open, setOpen] = useState(false)
+  //   const login = getLoginFn(useAuth0)
+  //   const logout = getLogoutFn(useAuth0)
+  //   const register = getRegisterFn(useAuth0)
+  const gardenId = useSelector(globalState => globalState.user?.gardenId)
+
+  function handleRegister (event) {
+    // event.preventDefault()
+    // register()
+  }
+
+  function handleLogin (event) {
+    // event.preventDefault()
+    // login()
+  }
+
+  function handleLogoff (event) {
+    // event.preventDefault()
+    // logout()
+  }
+
+  const toggleMenu = () => {
+    setOpen(!open)
+  }
+
+  return (
+    <nav className="nav" >
+      {open && <div className='nav-menu-toggle' onClick={toggleMenu}>
+        <Link to="/" className='nav-link'>Home</Link>
+
+        {/* <IfNotAuthenticated> */}
+        <a href="/" onClick={handleLogin} className='nav-link'>Sign in</a>
+        <a href="/" onClick={handleRegister} className='nav-link'>Register</a>
+        {/* </IfNotAuthenticated> */}
+
+        {/* <IfAuthenticated> */}
+        <Link to="/profile" className='nav-link'>My Profile</Link>
+        <a href="/" onClick={handleLogoff} className='nav-link'>Log out</a>
+        {/* </IfAuthenticated> */}
+
+        {/* <div className='close-btn' onClick={toggleMenu} ><IoClose/></div> */}
+      </div>
+      }
+      {!open && <div className='nav-menu'>
+        <Link to="/" className='nav-link'>Home</Link>
+
+        {/* <IfAuthenticated> */}
+        <Link to={`/gardens/${gardenId}`} className='nav-link'>My Garden</Link>
+        <Link to="/profile" className='nav-link'>My Profile</Link>
+        <a href="/" onClick={handleLogoff} className='nav-link'>Log out</a>
+        {/* </IfAuthenticated> */}
+
+        {/* <IfNotAuthenticated> */}
+        <a href="/" onClick={handleLogin} className='nav-link'>Sign in</a>
+        <a href="/" onClick={handleRegister} className='nav-link'>Register</a>
+        {/* </IfNotAuthenticated> */}
+
+        {/* <div className='hamburger' onClick={toggleMenu} ><GiHamburgerMenu/></div> */}
+      </div>
+      }
+    </nav>
+  )
+}
