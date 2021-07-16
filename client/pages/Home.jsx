37849ebdb93bd55a3afc90aaeb1c4b84bf7e97f1
Zachary Choi <zach7choi@gmail.com>
2021-07-16 17:35:04 +1200

    commit after pull from dev

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -6,6 +6,7 @@ export default function Home () {
     <>
       <main className='container'>
         <h1 className='page-title'>Welcome to getFood</h1>
+        <h1 className='testBlue'>Test</h1>
         <p>A food conserving project by getFood</p>
         <p>Please log in to get started, or checkout our About page</p>
       </main>
