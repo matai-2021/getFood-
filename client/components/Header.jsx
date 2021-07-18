ade3f77b692f1bed0a4ee15d1d1fe66596602698
Kritika Navani <kritikanavani99@gmail.com>
2021-07-18 16:19:10 +1200

    logo, button, border radius and more lil details

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import Nav from './Nav'
 // import WaitIndicator from '../components/WaitIndicator/WaitIndicator'
@@ -9,9 +9,9 @@ export default function Header () {
     <header className="header-container">
       <p>getFood! (Change to logo here, always stay up)</p>
 
-      {/* <Link className="logo-wrapper" to="/">
-  <img src='/images/gardenzLogo.svg' alt="gardenzlogo" className='logo-image' />
-</Link> */}
+      <Link className="logo-wrapper" to="/">
+        <img src='public/images/getFood-logo-01.svg' alt="getFood" className='logo-image' />
+      </Link>
       <Nav />
     </header>
 
