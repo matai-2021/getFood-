12d78a8183b29357d5207cc278470b815eeb8d29
krjw-00 <kriswd0@gmail.com>
2021-07-19 20:16:16 +1200

    tweak protection in knex file

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -19,7 +19,10 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
     pool: {
       min: 2,
       max: 10
