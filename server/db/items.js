84d24e8dc6251ccc33aaaaf52f25676f50e65bb7
krjw-00 <kriswd0@gmail.com>
2021-07-20 19:50:02 +1200

    conflict error

--- server/db/items.js
+++ server/db/items.js
@@ -56,10 +56,6 @@ function updateItem (updatedItem, db = connection) {
 
 function claimItem (claimedItem, db = connection) {
   const { id, isClaimed, claimedById } = claimedItem
-<<<<<<< HEAD
-=======
-  // console.log('isClaimed: ', isClaimed)
->>>>>>> 68d22f4882dfb1969b2ebf68366fad193ae95880
   return db('items')
     .where('id', id)
     .update({
