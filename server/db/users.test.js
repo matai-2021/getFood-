13b1e6f496283ff5b93144689680e6ad769f2a66
koko ono <nyswzt.a@gmail.com>
2021-07-20 12:57:22 +1200

    db test

--- server/db/users.test.js
+++ server/db/users.test.js
@@ -1,3 +1,47 @@
 const knex = require('knex')
 const config = require('./knexfile').test
-const testDB = knex(config)
+const testDb = knex(config)
+
+const db = require('./users')
+
+jest.setTimeout(10000)
+
+beforeAll(() => {
+  return testDb.migrate.latest()
+})
+
+beforeEach(() => {
+  return testDb.seed.run()
+})
+
+afterAll(() => {
+  return testDb.destroy()
+})
+
+describe('getAllUsers', () => {
+  it('returns the correct number of users', () => {
+    return db.getAllusers(testDb)
+      .then((users) => {
+        expect(users).toHaveLength(10)
+        return null
+      })
+  })
+})
+
+describe('getUserById', () => {
+  it('returns the chosen user', () => {
+    return db.getUserById(1, testDb)
+      .then(user => {
+        expect(user.id).toBe(1)
+        expect(user.name).toBe('Keisuke Tanaka')
+        expect(user.dateCreated).toBe('15/07 14:00')
+        expect(user.location).toBe('Glenfield, Auckland')
+        expect(user.isCompany).toBeFalsy()
+        expect(user.email).toBe('keisukyrocket@gmail.com')
+        expect(user.auth0Id).toBe('z2wRHxm1K1')
+        expect(user.companyName).toBe('null')
+        expect(user.phone).toBe('0211996925')
+        return null
+      })
+  })
+})
